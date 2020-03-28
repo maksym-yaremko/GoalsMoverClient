c355241b212ef1ab7bc50ba51d8d1943d4372ee2
Maksym Yaremko <maksym.yaremko@gmail.com>
2020-03-29 00:15:16 +0200

    add components for the main page

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import HorizontalList from './HorizontalList';
+import { Container, Row, Col } from 'reactstrap';
+import la from './mainPagePhoto.jpg'
+import './MainPage.css';
 
 const divstyle = {
     color : 'black',
     textAlign : 'center',
-    marginTop : '60px',
+    marginTop : '65px',
     fontSize: '55px',
-    fontWeight : '600'
+    fontWeight : '600',
+    paddingBottom:'40px'
 }
+const imagestyle ={
+    width:'auto',
+    height: '430px',
+    overflow: 'hidden'
+}
+
 
 function MainPage(){
     return (
         <div>
             <HorizontalList></HorizontalList>
         <div style={divstyle}>The #1 app for setting your goals</div>
+        <Container >
+            <Row>
+                <Col className="col1" md={{size:5,offset:1}}>
+                    <div style = {imagestyle}>
+                        <img src={la} alt="" />
+                    </div>
+                </Col>
+                <Col className="col2"  md={5}>
+                    <div>
+                        <a className="signup-button-facebook" href="/">Sign up with Facebook</a>
+                        <a className="signup-button-gmail" href="/">Sign up with Google</a>
+                    </div> 
+                    <div className="hr">
+                        OR
+                    </div>
+                    <div>
+                        <a className="signup-button-email" href="/signup">Sign up with Email</a>
+                    </div>
+                    <div className = "already-a-member">
+                        <p>Already a member?<a href="/login"> Log In</a></p>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
         </div>
+        
     );
 }
 
